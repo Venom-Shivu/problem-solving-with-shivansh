e08ddd0e91c5bc607ae2761ac8aea1716c5e562c
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2026-05-17 09:46:07 +0530

    Time: 67 ms (68.11%) | Memory: 20.3 MB (28.99%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -3,31 +3,26 @@ class Solution(object):
     def firstMissingPositive(self, nums):
 
         n = len(nums)
+        i = 0
+
+        while i < n:
+
+            v = nums[i]
+
+            if 1 <= v <= n and nums[v - 1] != v:
+
+                nums[i], nums[v - 1] = nums[v - 1], nums[i]
+
+            else:
+                i += 1
 
         i = 0
 
         while i < n:
 
-            val = nums[i]
-
-            # Correct position for current value
-            correct = val - 1
-
-            # Valid number and not already placed correctly
-            if 1 <= val <= n and nums[correct] != val:
-
-                # Faster manual swap
-                temp = nums[correct]
-                nums[correct] = val
-                nums[i] = temp
-
-            else:
-                i += 1
-
-        # Find first missing positive
-        for i in xrange(n):
-
             if nums[i] != i + 1:
                 return i + 1
+
+            i += 1
 
         return n + 1
