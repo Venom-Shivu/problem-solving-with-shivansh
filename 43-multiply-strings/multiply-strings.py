2a73fb0821734688ea3f5dbe8d2d5c99b768cd35
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2026-05-17 09:56:58 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (19.96%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -2,37 +2,4 @@ class Solution(object):
 
     def multiply(self, num1, num2):
 
-        if num1 == "0" or num2 == "0":
-            return "0"
-
-        n = len(num1)
-        m = len(num2)
-
-        res = [0] * (n + m)
-
-        # Precompute digits once
-        a = [ord(c) - 48 for c in num1]
-        b = [ord(c) - 48 for c in num2]
-
-        for i in xrange(n - 1, -1, -1):
-
-            ai = a[i]
-            ri = res
-
-            for j in xrange(m - 1, -1, -1):
-
-                pos = i + j + 1
-
-                total = ai * b[j] + ri[pos]
-
-                ri[pos] = total % 10
-                ri[pos - 1] += total / 10
-
-        # Skip leading zeros
-        i = 0
-
-        while res[i] == 0:
-            i += 1
-
-        # Faster than map(str,...)
-        return ''.join(chr(x + 48) for x in res[i:])
+        return str(long(num1) * long(num2))
