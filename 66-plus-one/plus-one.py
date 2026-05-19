cec7b7e56d806f956cfe633b9d389a7529f5d599
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2026-05-19 09:58:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (98.33%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,20 @@
+class Solution:
+    def plusOne(self, digits):
+        
+        n = len(digits)
+        
+        # Traverse from last digit
+        for i in range(n - 1, -1, -1):
+            
+            # If digit < 9
+            # just add 1 and return
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            
+            # If digit is 9
+            # becomes 0 due to carry
+            digits[i] = 0
+        
+        # If all digits were 9
+        return [1] + digits
