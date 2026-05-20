eae952d06d136c207500cb311f2e3c2d99c2fb12
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2026-05-20 11:19:14 +0530

    Time: 51 ms (58.74%) | Memory: 19.3 MB (72.96%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -1,27 +1,25 @@
 class Solution:
-    def isInterleave(self, s1, s2, s3):
-        if len(s1) + len(s2) != len(s3):
-            return False
-        
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         m, n = len(s1), len(s2)
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
-        
-        dp[0][0] = True
-        
-        # first column
-        for i in range(1, m + 1):
-            dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
-        
-        # first row
+
+        if m + n != len(s3):
+            return False
+
+        dp = [False] * (n + 1)
+        dp[0] = True
+
+        # First row initialization
         for j in range(1, n + 1):
-            dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
-        
-        # fill table
+            dp[j] = dp[j - 1] and s2[j - 1] == s3[j - 1]
+
         for i in range(1, m + 1):
+            dp[0] = dp[0] and s1[i - 1] == s3[i - 1]
+
             for j in range(1, n + 1):
-                dp[i][j] = (
-                    (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or
-                    (dp[i][j-1] and s2[j-1] == s3[i+j-1])
+                dp[j] = (
+                    (dp[j] and s1[i - 1] == s3[i + j - 1])
+                    or
+                    (dp[j - 1] and s2[j - 1] == s3[i + j - 1])
                 )
-        
-        return dp[m][n]
+
+        return dp[n]
