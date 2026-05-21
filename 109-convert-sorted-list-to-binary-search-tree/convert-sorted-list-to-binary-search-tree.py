a8018a7b00cbb0943b6b71663db7c153bb006bc9
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2026-05-21 19:54:22 +0530

    Time: 5 ms (62.86%) | Memory: 24.2 MB (17.77%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.py
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.py
@@ -1,22 +1,29 @@
 class Solution:
     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
-        nums = []
+        n = 0
+        curr = head
 
-        while head:
-            nums.append(head.val)
-            head = head.next
+        while curr:
+            n += 1
+            curr = curr.next
 
         def build(left, right):
+            nonlocal head
+
             if left > right:
                 return None
 
             mid = (left + right) // 2
 
-            root = TreeNode(nums[mid])
+            left_child = build(left, mid - 1)
 
-            root.left = build(left, mid - 1)
+            root = TreeNode(head.val)
+            root.left = left_child
+
+            head = head.next
+
             root.right = build(mid + 1, right)
 
             return root
 
-        return build(0, len(nums) - 1)
+        return build(0, n - 1)
