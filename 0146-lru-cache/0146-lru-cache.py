249c70a31373ea4e3bca4a8c2f2c935a1e703166
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2026-05-24 19:55:30 +0530

    Time: 110 ms (73.74%), Space: 76.7 MB (85.19%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,66 +1,26 @@
-class Node:
-    def __init__(self, key=0, value=0):
-        self.key = key
-        self.value = value
-        self.prev = None
-        self.next = None
-
+from collections import OrderedDict
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}   # key -> node
-
-        # Dummy nodes
-        self.left = Node()   # LRU
-        self.right = Node()  # MRU
-
-        self.left.next = self.right
-        self.right.prev = self.left
-
-    # Remove node from linked list
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next = nxt
-        nxt.prev = prev
-
-    # Insert node at MRU position
-    def insert(self, node):
-        prev = self.right.prev
-        nxt = self.right
-
-        prev.next = node
-        node.prev = prev
-        node.next = nxt
-        nxt.prev = node
+        self.cache = OrderedDict()
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            node = self.cache[key]
-
-            # Move accessed node to MRU
-            self.remove(node)
-            self.insert(node)
-
-            return node.value
-
-        return -1
-
+        if key not in self.cache:
+            return -1
+        
+        # Move accessed item to end (MRU)
+        self.cache.move_to_end(key)
+        return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
-
-        # If key exists, remove old node
+        
         if key in self.cache:
-            self.remove(self.cache[key])
+            self.cache.move_to_end(key)
 
-        node = Node(key, value)
-        self.cache[key] = node
-        self.insert(node)
+        self.cache[key] = value
 
-        # Capacity exceeded
+        # Remove LRU item
         if len(self.cache) > self.capacity:
-            # Remove LRU node
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
+            self.cache.popitem(last=False)
