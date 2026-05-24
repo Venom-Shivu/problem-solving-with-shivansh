297f90bea798e8e3478e61a6746fe3a65bd948d6
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2026-05-24 19:54:50 +0530

    Time: 152 ms (20.34%) | Memory: 78.8 MB (7.40%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,32 +1,66 @@
-from collections import OrderedDict
+class Node:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-
         self.capacity = capacity
+        self.cache = {}   # key -> node
 
-        self.cache = OrderedDict()
+        # Dummy nodes
+        self.left = Node()   # LRU
+        self.right = Node()  # MRU
 
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    # Remove node from linked list
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    # Insert node at MRU position
+    def insert(self, node):
+        prev = self.right.prev
+        nxt = self.right
+
+        prev.next = node
+        node.prev = prev
+        node.next = nxt
+        nxt.prev = node
 
     def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
 
-        if key not in self.cache:
-            return -1
+            # Move accessed node to MRU
+            self.remove(node)
+            self.insert(node)
 
-        self.cache.move_to_end(key)
+            return node.value
 
-        return self.cache[key]
+        return -1
 
 
     def put(self, key: int, value: int) -> None:
 
+        # If key exists, remove old node
         if key in self.cache:
+            self.remove(self.cache[key])
 
-            self.cache.move_to_end(key)
+        node = Node(key, value)
+        self.cache[key] = node
+        self.insert(node)
 
-        self.cache[key] = value
-
+        # Capacity exceeded
         if len(self.cache) > self.capacity:
-
-            self.cache.popitem(last=False)
+            # Remove LRU node
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
