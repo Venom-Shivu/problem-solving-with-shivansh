59477d7e827c41200a0945b01f54f5728c2de17d
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2026-05-26 11:54:38 +0530

    Time: 215 ms (17.03%), Space: 40.6 MB (88.22%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -9,68 +9,69 @@ class Solution:
         if not head or not head.next:
             return head
 
-        # Find length
-        length = 0
-        curr = head
-        while curr:
-            length += 1
-            curr = curr.next
+        # Get length
+        n = 0
+        node = head
+        while node:
+            n += 1
+            node = node.next
 
         dummy = ListNode(0)
         dummy.next = head
-        size = 1
 
-        while size < length:
+        step = 1
+
+        while step < n:
             prev = dummy
             curr = dummy.next
 
             while curr:
+
+                # Left list
                 left = curr
-                right = self.split(left, size)
-                curr = self.split(right, size)
 
-                mergedHead, mergedTail = self.merge(left, right)
+                # Right list
+                right = curr
+                for _ in range(step - 1):
+                    if right and right.next:
+                        right = right.next
 
-                prev.next = mergedHead
-                prev = mergedTail
+                if not right:
+                    break
 
-            size *= 2
+                curr = right.next
+                right.next = None
+                right = curr
+
+                # Next chunk
+                for _ in range(step - 1):
+                    if curr and curr.next:
+                        curr = curr.next
+
+                nextStart = None
+                if curr:
+                    nextStart = curr.next
+                    curr.next = None
+
+                # Merge
+                l1, l2 = left, right
+
+                while l1 and l2:
+                    if l1.val <= l2.val:
+                        prev.next = l1
+                        l1 = l1.next
+                    else:
+                        prev.next = l2
+                        l2 = l2.next
+                    prev = prev.next
+
+                prev.next = l1 if l1 else l2
+
+                while prev.next:
+                    prev = prev.next
+
+                curr = nextStart
+
+            step *= 2
 
         return dummy.next
-
-    # Split list after 'size' nodes
-    def split(self, head, size):
-        if not head:
-            return None
-
-        for _ in range(size - 1):
-            if head.next:
-                head = head.next
-            else:
-                break
-
-        second = head.next
-        head.next = None
-        return second
-
-    # Merge two sorted lists
-    def merge(self, l1, l2):
-        dummy = ListNode()
-        tail = dummy
-
-        while l1 and l2:
-            if l1.val <= l2.val:
-                tail.next = l1
-                l1 = l1.next
-            else:
-                tail.next = l2
-                l2 = l2.next
-
-            tail = tail.next
-
-        tail.next = l1 if l1 else l2
-
-        while tail.next:
-            tail = tail.next
-
-        return dummy.next, tail
