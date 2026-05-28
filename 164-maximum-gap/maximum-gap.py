991aeffd38657ff6f7c84a9d795e982159bc942a
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2026-05-28 19:13:43 +0530

    Time: 127 ms (95.33%) | Memory: 31.8 MB (81.38%) - LeetSync

--- 164-maximum-gap/maximum-gap.py
+++ 164-maximum-gap/maximum-gap.py
@@ -6,47 +6,18 @@ class Solution:
         if n < 2:
             return 0
 
-        mn = min(nums)
-        mx = max(nums)
-
-        if mn == mx:
-            return 0
-
-        size = max(1, (mx - mn) // (n - 1))
-        cnt = (mx - mn) // size + 1
-
-        buckets = [[-1, -1] for _ in range(cnt)]
-
-        # fill buckets
-        for x in nums:
-
-            idx = (x - mn) // size
-
-            b = buckets[idx]
-
-            if b[0] == -1:
-                b[0] = x
-                b[1] = x
-            else:
-                if x < b[0]:
-                    b[0] = x
-                elif x > b[1]:
-                    b[1] = x
+        nums.sort()
 
         ans = 0
-        prev = mn
+        prev = nums[0]
 
-        # compute gaps
-        for bmin, bmax in buckets:
+        for i in range(1, n):
 
-            if bmin == -1:
-                continue
-
-            gap = bmin - prev
+            gap = nums[i] - prev
 
             if gap > ans:
                 ans = gap
 
-            prev = bmax
+            prev = nums[i]
 
         return ans
