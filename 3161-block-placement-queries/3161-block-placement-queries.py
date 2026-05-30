4043ae38763d3d609bb699ec5be3fe1b6a0f4e4a
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2026-05-30 08:55:44 +0530

    Time: 3977 ms (44.72%), Space: 79.2 MB (61.79%) - LeetHub

--- 3161-block-placement-queries/3161-block-placement-queries.py
+++ 3161-block-placement-queries/3161-block-placement-queries.py
@@ -2,51 +2,62 @@ from sortedcontainers import SortedList
 
 class Solution:
     def getResults(self, queries):
+
         mx = max(q[1] for q in queries)
 
-        obstacles = SortedList([0, mx + 1])
+        obs = SortedList([0, mx + 1])
 
-        # Add all obstacles first
         for q in queries:
             if q[0] == 1:
-                obstacles.add(q[1])
+                obs.add(q[1])
 
-        n = mx + 3
+        size = mx + 2
 
-        seg = [0] * (4 * n)
+        n = 1
+        while n < size:
+            n <<= 1
 
-        def update(idx, val, node, l, r):
-            if l == r:
-                seg[node] = val
-                return
+        seg = [0] * (2 * n)
 
-            mid = (l + r) // 2
+        def update(pos, val):
+            p = pos + n
+            seg[p] = val
 
-            if idx <= mid:
-                update(idx, val, node * 2, l, mid)
-            else:
-                update(idx, val, node * 2 + 1, mid + 1, r)
+            p >>= 1
 
-            seg[node] = max(seg[node * 2], seg[node * 2 + 1])
+            while p:
+                left = seg[p << 1]
+                right = seg[p << 1 | 1]
+                seg[p] = left if left > right else right
+                p >>= 1
 
-        def query(L, R, node, l, r):
-            if L > r or R < l:
-                return 0
+        def query(l, r):
+            l += n
+            r += n
 
-            if L <= l and r <= R:
-                return seg[node]
+            res = 0
 
-            mid = (l + r) // 2
+            while l <= r:
 
-            return max(
-                query(L, R, node * 2, l, mid),
-                query(L, R, node * 2 + 1, mid + 1, r)
-            )
+                if l & 1:
+                    if seg[l] > res:
+                        res = seg[l]
+                    l += 1
 
-        arr = list(obstacles)
+                if not (r & 1):
+                    if seg[r] > res:
+                        res = seg[r]
+                    r -= 1
+
+                l >>= 1
+                r >>= 1
+
+            return res
+
+        arr = list(obs)
 
         for i in range(1, len(arr)):
-            update(arr[i], arr[i] - arr[i - 1], 1, 0, n)
+            update(arr[i], arr[i] - arr[i - 1])
 
         ans = []
 
@@ -54,13 +65,19 @@ class Solution:
 
             if q[0] == 2:
 
-                x, sz = q[1], q[2]
+                x = q[1]
+                sz = q[2]
 
-                idx = obstacles.bisect_right(x)
-                prev = obstacles[idx - 1]
+                idx = obs.bisect_right(x)
 
-                best = query(0, prev, 1, 0, n)
-                best = max(best, x - prev)
+                prev = obs[idx - 1]
+
+                best = query(0, prev)
+
+                gap = x - prev
+
+                if gap > best:
+                    best = gap
 
                 ans.append(best >= sz)
 
@@ -68,14 +85,15 @@ class Solution:
 
                 x = q[1]
 
-                idx = obstacles.index(x)
+                idx = obs.bisect_left(x)
 
-                left = obstacles[idx - 1]
-                right = obstacles[idx + 1]
+                left = obs[idx - 1]
+                right = obs[idx + 1]
 
-                update(right, right - left, 1, 0, n)
-                update(x, 0, 1, 0, n)
+                update(right, right - left)
+                update(x, 0)
 
-                obstacles.remove(x)
+                obs.remove(x)
 
-        return ans[::-1]
+        ans.reverse()
+        return ans
