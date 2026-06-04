c32c83b8a5c06a304253524b1390de9145427088
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2026-06-04 21:12:07 +0530

    Time: 454 ms (21.73%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,8 +1,7 @@
-/* Write your T-SQL query statement below */
-SELECT 
-    p.firstName, 
-    p.lastName, 
-    a.city, 
-    a.state
-FROM Person p WITH (NOLOCK)
-LEFT JOIN Address a WITH (NOLOCK) ON p.personId = a.personId;
+SELECT Person.firstName,
+       Person.lastName,
+       Address.city,
+       Address.state
+FROM Person
+LEFT JOIN Address
+ON Person.personId = Address.personId;
