1b99b314eb76e4c8a2770660d8f4709bc0b166c7
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2026-06-12 15:11:26 +0530

    Time: 855 ms (84.91%) | Memory: 178.1 MB (45.28%) - LeetSync

--- 3842-number-of-ways-to-assign-edge-weights-ii/number-of-ways-to-assign-edge-weights-ii.py
+++ 3842-number-of-ways-to-assign-edge-weights-ii/number-of-ways-to-assign-edge-weights-ii.py
@@ -1,75 +1,66 @@
-from typing import List
-
+MX = 100001
+mod = 10**9+7
+fac = [1]*MX
+for i in range(1,MX):
+    fac[i] = fac[i-1]*(i)%mod
+inv = [1]*MX
+inv[-1] = pow(fac[-1],mod-2,mod)
+for i in range(MX-2,-1,-1):
+    inv[i] = inv[i+1]*(i+1)%mod
 class Solution:
     def assignEdgeWeights(self, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
-        # n nodes, labeled 1 to n. 
-        # n-1 edges in a tree means n = len(edges) + 1
-        n = len(edges) + 1
-        adj = [[] for _ in range(n + 1)]
-        for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
-            
-        # 1. BFS to compute depths and immediate parents
-        depth = [-1] * (n + 1)
-        parent = [0] * (n + 1)
-        depth[1] = 0
-        queue = [1]
+        n = len(edges)+1
+        deep = [0]*(n+1)
+        g = [[] for _ in range(n+1)]
+        q = [[] for _ in range(n+1)]
+        ans = [0]*(len(queries))
+        vis = [False] * (n+1)
+        father = [i for i in range(n+1)]
+        for u,v in edges:
+            g[u].append(v)
+            g[v].append(u)
+        for idx,[u,v] in enumerate(queries):
+            q[u].append((v,idx))
+            q[v].append((u,idx))
         
-        # Standard iterative BFS for speed and to avoid recursion depth issues
-        for u in queue:
-            for v in adj[u]:
-                if depth[v] == -1:
-                    depth[v] = depth[u] + 1
-                    parent[v] = u
-                    queue.append(v)
-                    
-        # 2. Binary Lifting for LCA (Lowest Common Ancestor)
-        LOG = 17 # 2^17 > 10^5
-        up = [parent] # up[0] is immediate parents
-        for i in range(1, LOG):
-            prev_up = up[-1]
-            # up[i][v] = up[i-1][up[i-1][v]]
-            curr_up = [prev_up[prev_up[v]] for v in range(n + 1)]
-            up.append(curr_up)
+        def find(x):
+            while father[x]!=x:
+                father[x] = father[father[x]]
+                x = father[x]
+            return father[x]
+        
+        def tarjan(u,parent):
+            vis[u] = True
+            for v in g[u]:
+                if v == parent:
+                    continue
+                tarjan(v,u)
+                father[v] = u
             
-        def get_lca(u, v):
-            if depth[u] < depth[v]:
-                u, v = v, u
-            
-            # Lift u to same depth as v
-            diff = depth[u] - depth[v]
-            for i in range(LOG):
-                if (diff >> i) & 1:
-                    u = up[i][u]
-            
-            if u == v:
-                return u
-            
-            # Lift both until parents match
-            for i in range(LOG - 1, -1, -1):
-                if up[i][u] != up[i][v]:
-                    u = up[i][u]
-                    v = up[i][v]
-            return up[0][u]
-
-        # 3. Precompute powers of 2 for fast queries
-        MOD = 10**9 + 7
-        pow2 = [1] * (n + 1)
-        for i in range(1, n + 1):
-            pow2[i] = (pow2[i-1] * 2) % MOD
-            
-        # 4. Answer Queries
-        results = []
-        for u, v in queries:
-            if u == v:
-                results.append(0)
-                continue
-            
-            lca = get_lca(u, v)
-            # Path length L
-            L = depth[u] + depth[v] - 2 * depth[lca]
-            # Ways to assign weights such that cost is odd is 2^(L-1)
-            results.append(pow2[L - 1])
-            
-        return results
+            for node,idx in q[u]:
+                if vis[node]:
+                    ans[idx] = find(node)
+        tarjan(1,0)
+        def f(u,fa,d):
+            nonlocal deep
+            deep[u] = d
+            for v in g[u]:
+                if v==fa:continue
+                f(v,u,d+1)
+        f(1,0,0)
+        res = [0]*len(queries)
+        def comb(n,a):
+            return fac[n]*inv[a]*inv[n-a]%mod
+        def f1(x):
+            ret = 0
+            for i in range(1,x+1,2):
+                ret = (ret + comb(x,i))%mod
+            return ret
+        for i,[a,b] in enumerate(queries):
+            lca = ans[i]
+            x = deep[a]+deep[b] - 2*deep[lca]
+            if x == 0:
+                res[i] = 0
+            else:
+                res[i] = pow(2, x-1, mod)
+        return res
