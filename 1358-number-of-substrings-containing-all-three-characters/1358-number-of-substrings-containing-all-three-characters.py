25346e0431a970403ad8ddb9f4241b0bf1f943b7
Shivansh Yadav <mrshivusinghyadav@gmail.com>
2026-06-30 10:42:42 +0530

    Time: 6 ms (100%), Space: 32.5 MB (15.25%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,16 +1,24 @@
-class Solution:
-    def numberOfSubstrings(self, s):
-        count = {'a': 0, 'b': 0, 'c': 0}
-        left = 0
-        res = 0
+
+import numpy
+
+ids = bytearray(256)
+ids[98:100] = 1, 2
+
+def numberOfSubstrings(
+    s: str, ids=bytes(ids),
+    indices=numpy.arange(1, 50_001, dtype=numpy.uint16)
+) -> int:
+    s = numpy.frombuffer(s.encode().translate(ids), dtype=numpy.uint8)
+    places = numpy.zeros((3, s.size + 1), dtype=numpy.uint16)
+    indices = indices[:s.size]
+    places[s, indices] = indices
+    return numpy.maximum.accumulate(places, axis=1, out=places)\
+        .min(axis=0)\
+        .sum()\
+        .item()   
+
+
+Solution = repeat(namedtuple('Solution', ('numberOfSubstrings',))(
+    numberOfSubstrings
+)).__next__
         
-        for right in range(len(s)):
-            count[s[right]] += 1
-            
-            # shrink window while valid
-            while count['a'] > 0 and count['b'] > 0 and count['c'] > 0:
-                res += len(s) - right
-                count[s[left]] -= 1
-                left += 1
-        
-        return res
